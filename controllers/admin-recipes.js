baaf4402c6358bde94c471d29a38fd985788a873
João Victor Rodrigues <joao15.vi.rodrigues@gmail.com>
2020-12-22 17:59:56 -0300

    Desafio 4 foodfy - página index adm ok

---
+++ controllers/admin-recipes.js
@@ -0,0 +1,33 @@
+const fs = require('fs')
+const data = require('../data.json')
+
+exports.index = function(req, res) {
+
+    return res.render('area-adm/index', { recipes: data.recipes })
+}
+exports.create = function(req, res) {
+    return res.render('area-adm/about')
+}
+
+exports.show = function(req, res) {
+    const { id } = req.params
+
+    const foundRecipe = data.recipes.find((recipe) => { return recipe.id == id })
+
+    if (!foundRecipe)
+        return res.send('Receita not Found')
+
+    return res.render('area-adm/recipe', { recipe: foundRecipe })
+}
+exports.edit = function(req, res) {
+    return res.render('area-adm/recipe', { recipe: data.recipes[0] })
+}
+exports.post = function(req, res) {
+    return res.redirect('/admin/recipes')
+}
+exports.put = function(req, res) {
+    return res.redirect('/admin/recipes/0')
+}
+exports.delete = function(req, res) {
+    return res.redirect('/admin/recipes')
+}
--- routes.js
+++ routes.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const routes = express.Router()
 const recipes = require('./controllers/recipes')
+const admin = require('./controllers/admin-recipes')
 
 
 routes.get('/', recipes.index)
 routes.get('/about', recipes.about)
 routes.get('/recipes', recipes.showAll)
 routes.get('/recipe/:id', recipes.show)
+
+
+routes.get("/admin/recipes", admin.index); // Mostrar a lista de receitas
+routes.get("/admin/recipes/create", admin.create); // Mostrar formulário de nova receita
+routes.get("/admin/recipes/:id", admin.show); // Exibir detalhes de uma receita
+routes.get("/admin/recipes/:id/edit", admin.edit); // Mostrar formulário de edição de receita
+
+routes.post("/admin/recipes", admin.post); // Cadastrar nova receita
+routes.put("/admin/recipes", admin.put); // Editar uma receita
+routes.delete("/admin/recipes", admin.delete); // Deletar uma receita
+
+
 module.exports = routes
