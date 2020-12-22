ec74eb56880d3e6541e3db286c11019dd5d1fbbe
João Victor Rodrigues <joao15.vi.rodrigues@gmail.com>
2020-12-22 19:01:42 -0300

    Desafio 4 foodfy - página index adm OK - página show recipe adm OK

--- controllers/admin-recipes.js
+++ controllers/admin-recipes.js
@@ -20,7 +20,13 @@ exports.show = function(req, res) {
     return res.render('area-adm/recipe', { recipe: foundRecipe })
 }
 exports.edit = function(req, res) {
-    return res.render('area-adm/recipe', { recipe: data.recipes[0] })
+    const { id } = req.params
+
+    const foundRecipe = data.recipes.find((recipe) => { return recipe.id == id })
+
+    if (!foundRecipe)
+        return res.send('Receita not Found')
+    return res.render('area-adm/edit', { recipe: foundRecipe })
 }
 exports.post = function(req, res) {
     return res.redirect('/admin/recipes')
