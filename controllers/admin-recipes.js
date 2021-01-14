087ade7bfcb4d645576387e17674224187b1f811
João Victor Rodrigues <joao15.vi.rodrigues@gmail.com>
2021-01-13 21:18:03 -0300

    implementação - metodo post (OK)

--- controllers/admin-recipes.js
+++ controllers/admin-recipes.js
@@ -29,7 +29,40 @@ exports.edit = function(req, res) {
     return res.render('area-adm/edit', { recipe: foundRecipe })
 }
 exports.post = function(req, res) {
-    return res.redirect('/admin/recipes')
+    const keys = Object.keys(req.body)
+    for (key of keys) {
+        if (req.body[key] == "")
+            return res.send("Por favor, preencha todos os campos")
+    }
+
+    let { image, title, author, ingredients, preparation, information } = req.body
+
+    let id = 1
+    const lastRecipes = data.recipes[data.recipes.length - 1]
+
+    if (lastRecipes) {
+        id = lastRecipes.id + 1
+    }
+
+    data.recipes.push({
+        id,
+        image,
+        title,
+        author: `por ${author}`,
+        ingredients,
+        preparation,
+        information
+
+    })
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+
+            if (err)
+                return res.send("Erro ao escrever arquivo")
+
+            return res.redirect("/admin/recipes")
+
+        })
+        // return res.send(req.body)
 }
 exports.put = function(req, res) {
     return res.redirect('/admin/recipes/0')
