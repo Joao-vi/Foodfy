00adbea2d293d4b0cbe07794706e84bcfa14738c
João Victor Rodrigues <joao15.vi.rodrigues@gmail.com>
2021-01-19 21:09:50 -0300

    implementação - função para adicionar mais de ingrediente e modo de preparo - função acima não funciona em conjunto com a de esconder e mostrar - quando  salva a receita a página fica recarregando  sem parar

--- front-end/public/script.js
+++ front-end/public/script.js
@@ -14,6 +14,7 @@ for (let i = 0; i < hides.length; i++) {
         if (!info[i].classList.contains('hidden')) {
             info[i].classList.add('hidden')
             hides[i].textContent = 'Mostrar'
+            console.log("Teste testando")
         } else {
             info[i].classList.remove('hidden')
             hides[i].textContent = 'Esconder'
@@ -23,4 +24,29 @@ for (let i = 0; i < hides.length; i++) {
 
 function return_index() {
     window.location.href = '/'
+}
+
+/* Function add new preparation and ingredient */
+
+const addIgredient = document.querySelector(".add-ingredient")
+addIgredient.addEventListener('click', function() { addIngredient("ingredient") })
+
+const addPreparation = document.querySelector(".add-preparation")
+addPreparation.addEventListener('click', function() { addIngredient("preparation") })
+
+function addIngredient(item) {
+    console.log(item)
+
+    const ingredients = document.querySelector(`.${item}`);
+    const fieldContainer = document.querySelectorAll(`#${item}`);
+
+    // Realiza um clone do último ingrediente adicionado
+    const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true);
+
+    // Não adiciona um novo input se o último tem um valor vazio
+    if (newField.children[0].value == "") return false;
+
+    // Deixa o valor do input vazio
+    newField.children[0].value = "";
+    ingredients.appendChild(newField);
 }
